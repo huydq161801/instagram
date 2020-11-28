6624129c9d5faee276fa1e61a6eb0bbb871461e0
huydq161801 <55663815+huydq161801@users.noreply.github.com>
2020-11-29 00:50:47 +0700

    hrlo
    rfgff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts.includes(:photos, :likes, :comments)
+    @posts = @user.posts.includes(:photos)
     @saved = Post.joins(:bookmarks).where(user_id: current_user.id).
-      includes(:photos, :likes, :comments) if @user == current_user
+      includes(:photos) if @user == current_user
   end
 end
